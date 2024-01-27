fe5042e48ea3ed845d2c781bf1be68d08fad0acd
Donnie Goodson <donnie86@gmail.com>
2024-01-26 22:58:08 -0800

    Initialize CheckSplitter

---
+++ CheckSplitter/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CheckSplitter
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ CheckSplitter/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CheckSplitter
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
